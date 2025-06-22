c9a9733dfa3836e2ae08fb0b7a06c187666aa39b
aAlmazQW <119169551+aAlmazQW@users.noreply.github.com>
2025-06-22 22:39:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ async def track_loop(bot: Bot):
             last_status = "playing"
             last_track_id = track["id"]
             try:
-                text = f" {track['title']} — {track['artists']}"
+                text = f" {track['**title**']} — {track['__artists__']}"
                 save_track_to_history(track['title'], track['artists'])
 
                 keyboard = [[InlineKeyboardButton("🎧 Слушать в Я.Музыке", url=track["link"])]]
